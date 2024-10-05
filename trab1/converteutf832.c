7df9e74d3258a2ef03c15d3deb3d0ccf1bb44a07
duduardo45 <146144048+duduardo45@users.noreply.github.com>
2024-10-05 20:48:46 -0300

    adiciona arquivos para teste

--- trab1/converteutf832.c
+++ trab1/converteutf832.c
@@ -2,3 +2,15 @@
 
 int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida);
 int convUtf32p8(FILE *arquivo_entrada, FILE *arquivo_saida);
+
+
+int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida) {
+
+    return 0;
+}
+
+int convUtf32p8(FILE *arquivo_entrada, FILE *arquivo_saida) {
+
+    return 0;
+}
+
