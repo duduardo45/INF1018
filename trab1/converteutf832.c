e2cc650eea7f2839d098ce94baa07a912f055444
duduardo45 <146144048+duduardo45@users.noreply.github.com>
2024-10-05 21:56:10 -0300

    inicio do trab

--- trab1/converteutf832.c
+++ trab1/converteutf832.c
@@ -6,6 +6,10 @@ int convUtf32p8(FILE *arquivo_entrada, FILE *arquivo_saida);
 
 int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida) {
 
+    int byte;
+    while ((byte = getc(arquivo_entrada)) != EOF) {
+        printf("Byte: 0x%x\n", byte);  // Print the byte in hexadecimal form
+    }
     return 0;
 }
 
@@ -14,3 +18,10 @@ int convUtf32p8(FILE *arquivo_entrada, FILE *arquivo_saida) {
     return 0;
 }
 
+int main(void) {
+
+    FILE* e = fopen("utf8.txt", "rb");
+    convUtf8p32(e,NULL);
+
+    return 0;
+}
