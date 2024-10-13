f920e26f4dfaad60606f630ef10371d825097fa0
duduardo45 <146144048+duduardo45@users.noreply.github.com>
2024-10-13 20:05:42 -0300

    relatorio e pequenas correcoes

--- trab1/converteutf832.c
+++ trab1/converteutf832.c
@@ -1,3 +1,8 @@
+/*
+trabalho feito sozinho por:
+Eduardo Eugênio de Souza 2310822 3WC
+*/
+
 #include <stdio.h>
 
 int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida);
@@ -10,11 +15,11 @@ typedef unsigned int uint;
 int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida) {
 
     // por estar usando um computador já little endian, o BOM já está na versão correta em memória
-    unsigned int byte = BOM;
+    uint byte = BOM;
     int utf32_char;
     int i = 0;
     unsigned char ajuste;
-    fwrite(&byte, sizeof( unsigned int), 1, arquivo_saida);
+    fwrite(&byte, sizeof(uint), 1, arquivo_saida);
     if (ferror(arquivo_saida)) {
         fprintf(stderr, "Erro de gravacao.\n");
         return -1;
@@ -39,6 +44,10 @@ int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida) {
             utf32_char = (byte>>(i+1)) & ajuste;
             for (;i;i--) {
                 byte = getc(arquivo_entrada);
+                if (ferror(arquivo_entrada)) {
+                    fprintf(stderr, "Erro de leitura.\n");
+                    return -1;
+                }
                 utf32_char <<=6;
                 utf32_char |= byte & 0x3F;
             }
@@ -47,7 +56,7 @@ int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida) {
             utf32_char = byte;
         }
         // escreve de acordo com a arquitetura do computador usado, neste caso little endian
-        fwrite(&utf32_char, sizeof(unsigned int), 1, arquivo_saida); 
+        fwrite(&utf32_char, sizeof(uint), 1, arquivo_saida); 
         if (ferror(arquivo_saida)) {
             fprintf(stderr, "Erro de gravacao.\n");
             return -1;
@@ -59,8 +68,7 @@ int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida) {
 uint inverte_int(uint n) {
     unsigned char bytes[4];
     for (int i=0;i<4;i++) {
-        bytes[i] = (n >> (8*i)); // & 0xFF;
-        // printf("%x %x %x\n", bytes[i],(n << (8*i)),n);
+        bytes[i] = (n >> (8*i)); // & 0xFF; desnecessário por questão de tipo usado
     }
     n = (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | (bytes[3]);
     return n;
@@ -95,32 +103,32 @@ int convUtf32p8(FILE *arquivo_entrada, FILE *arquivo_saida) {
         if (big_endian) char32 = inverte_int(char32);
         
         for (i = 31; i;i--) if ((char32 >> i) & 1) break;
-        i++;
+        i++; // conta a quantidade de bits não nulos
 
         if (i < 8) {
             out[0] = char32;
             fwrite(out,sizeof(unsigned char), 1, arquivo_saida);
         }
         else if (i < 12) {
-            out[0] = 0b11000000 | ((char32 >> 6) & 0b00011111);
-            out[1] = 0b10000000 | (char32 & 0b00111111);
+            out[0] = 0b11000000 | ((char32 >>  6) & 0b00011111);
+            out[1] = 0b10000000 | ( char32        & 0b00111111);
             fwrite(out,sizeof(unsigned char), 2, arquivo_saida);
         }
         else if (i < 17) {
             out[0] = 0b11100000 | ((char32 >> 12) & 0b00001111);
-            out[1] = 0b10000000 | ((char32 >> 6) & 0b00111111);
-            out[2] = 0b10000000 | (char32 & 0b00111111);
+            out[1] = 0b10000000 | ((char32 >>  6) & 0b00111111);
+            out[2] = 0b10000000 | ( char32        & 0b00111111);
             fwrite(out,sizeof(unsigned char), 3, arquivo_saida);
         }
         else if (i < 22) {
             out[0] = 0b11110000 | ((char32 >> 18) & 0b00000111);
             out[1] = 0b10000000 | ((char32 >> 12) & 0b00111111);
-            out[2] = 0b10000000 | ((char32 >> 6) & 0b00111111);
-            out[3] = 0b10000000 | (char32 & 0b00111111);
+            out[2] = 0b10000000 | ((char32 >>  6) & 0b00111111);
+            out[3] = 0b10000000 | ( char32        & 0b00111111);
             fwrite(out,sizeof(unsigned char), 4, arquivo_saida);
         }
         else {
-            printf("caractere nao representavel por utf-8\n");
+            fprintf(stderr, "Caractere nao representavel por utf-8.\n");
             return -1;
         }
         if (ferror(arquivo_saida)) {
@@ -131,14 +139,14 @@ int convUtf32p8(FILE *arquivo_entrada, FILE *arquivo_saida) {
     return 0;
 }
 
-int cria_big_endian(FILE* entrada, FILE* saida) {
-    uint char32;
-    while (fread(&char32, sizeof(uint), 1, entrada) == 1) {
-        char32 = inverte_int(char32);
-        fwrite(&char32,4,1,saida);
-    }
-    return 0;
-}
+// int cria_big_endian(FILE* entrada, FILE* saida) {
+//     uint char32;
+//     while (fread(&char32, sizeof(uint), 1, entrada) == 1) {
+//         char32 = inverte_int(char32);
+//         fwrite(&char32,4,1,saida);
+//     }
+//     return 0;
+// }
 
 // int main(void) {
 
