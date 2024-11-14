b30fb80a6cfa6af8d7b2fce8049ad1d80d0bf71f
duduardo45 <146144048+duduardo45@users.noreply.github.com>
2024-11-13 22:33:20 -0300

    adiciona esqueleto do trab2

---
+++ trab2/cria_func.c
@@ -0,0 +1,6 @@
+/* Bruno Azambuja Carvalho 2310956 3WC */
+/* Eduardo Eugênio de Souza 2310822 3WC */
+#include <stdio.h>
+#include "cria_func.h"
+
+void cria_func (void* f, DescParam params[], int n, unsigned char codigo[]);
