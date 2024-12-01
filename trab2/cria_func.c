0ef7b6a36811823f3278fd5337ff5b7015fb31ef
duduardo45 <146144048+duduardo45@users.noreply.github.com>
2024-12-01 11:51:52 -0300

    versao de funcoes sem parametro funcionando

--- trab2/cria_func.c
+++ trab2/cria_func.c
@@ -3,10 +3,70 @@
 #include "cria_func.h"
 #include <stdio.h>
 
-void cria_func(void *f, DescParam params[], int n, unsigned char codigo[]) {
-  unsigned char assembly[23] = {0x55, 0x48, 0x89, 0xe5, 0x48, 0x83, 0xec, 0x10, 0x48, 0x89, 0x7d, 0xf8, 0xff, 0xd7, 0xf8,0x48, 0x89, 0xec, 0x5d, 0xc3};
-
-  for (int i = 0; i < 23; i++) {
-    codigo[i] = assembly[i];
+void dump (void *p, int n) {
+  unsigned char *p1 = p;
+  while (n--) {
+    printf("%p - %02x\n", p1, *p1);
+    p1++;
   }
+}
+
+
+// void cria_func(void *f, DescParam params[], int n, unsigned char codigo[]) {
+// //   unsigned char assembly[23] = {0x55, 0x48, 0x89, 0xe5, 0x48, 0x83, 0xec, 0x10, 0x48, 0x89, 0x7d, 0xf8, 0xff, 0xd7, 0xf8,0x48, 0x89, 0xec, 0x5d, 0xc3};
+// //   unsigned char assembly[23] = {0x55, 0x48, 0x89, 0xe5, 0x48, 0x83, 0xec, 0x10, 0x48, 0x89, 0x7d, 0xf8, 0xff, 0xd7, 0x48, 0x8b, 0x7d, 0xf8, 0x48, 0x89, 0xec, 0x5d, 0xc3};
+//   unsigned char assembly[30] = {
+//     0x55, 
+//     0x48, 0x89, 0xe5, 
+//     0x48, 0x83, 0xec, 0x10,
+//     0x48, 0x89, 0x7d, 0xf8, 
+//     0x48, 0xc7, 0xc0, 0x55, 0x48, 0x89, 0xe5, // alterar estes bytes!!! a ideia é por os bytes de f no lugar destes
+//     0xff, 0xd0, 
+//     0x48, 0x8b, 0x7d, 0xf8, 
+//     0x48, 0x89, 0xec, 
+//     0x5d, 
+//     0xc3
+//   };
+
+//   // percorrer o params[] e alterar o assembly[] para usar os valores dados em cada parametro
+
+//   for (int i = 0; i < 30; i++) {
+//     codigo[i] = assembly[i];
+//   }
+// }
+
+
+
+#include "cria_func.h"
+
+void cria_func(void *f, DescParam params[], int n, unsigned char codigo[]) {
+    unsigned char *p = codigo;
+
+    // Prólogo
+    *p++ = 0x55;                  // push   %rbp
+    *p++ = 0x48; *p++ = 0x89; *p++ = 0xe5; // mov    %rsp, %rbp
+
+    // Configurar parâmetros
+    for (int i = 0; i < n; i++) {
+        switch (params[i].orig_val) {
+            case PARAM:
+                // Usar registradores na ordem rdi, rsi, rdx
+                break;
+            case FIX:
+                // Mover valor constante para o registrador correspondente
+                break;
+            case IND:
+                // Carregar valor indireto de memória
+                break;
+        }
+    }
+
+    // Chamada à função original
+    *p++ = 0x48; *p++ = 0xb8;             // mov $endereço, %rax
+    *((void **)p) = f; p += 8;
+    *p++ = 0xff; *p++ = 0xd0;             // call *%rax
+
+    // Epílogo
+    *p++ = 0xc9;                  // leave
+    *p++ = 0xc3;                  // ret
 }
